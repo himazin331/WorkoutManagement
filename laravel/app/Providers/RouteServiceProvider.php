2491da5f1fb7e318060037910e5146d8e628c56b
himazin331 <sion20022013@gmail.com>
2021-09-07 04:21:18 +0900

    RouteServiceProvider

---
+++ laravel/app/Providers/RouteServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+//* ルーティングサービスプロパイダ */
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /* デフォルトリダイレクト先 */
+    public const HOME = '/index.php';
+
+    /* ルーティング設定呼び出し先の設定など */
+    public function boot()
+    {
+        $this->configureRateLimiting();
+        
+        $this->routes(function () {
+            Route::prefix('api')
+                ->middleware('api')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/web.php'));
+        });
+
+        /* 全てのビューに変数を渡す */
+        view()->composer('*', function ($view) {
+            $user = Auth::user();
+            $view->with(compact('user'));
+        });
+    }
+
+    /**
+     * Configure the rate limiters for the application.
+     *
+     * @return void
+     */
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+        });
+    }
+}
