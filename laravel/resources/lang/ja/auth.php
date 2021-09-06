98c99694d16d43a8a340b3e8cc37f9cf26cca972
himazin331 <sion20022013@gmail.com>
2021-09-07 04:25:50 +0900

    Auth返却メッセージ

---
+++ laravel/resources/lang/ja/auth.php
@@ -0,0 +1,11 @@
+<?php
+//* 認証返却メッセージ */
+
+return [
+    // ログイン失敗時
+    'failed' => 'ユーザIDもしくはパスワードが正しくありません。',
+    // パスワード変更失敗時
+    'password' => 'The provided password is incorrect.',
+    // ログイン試行回数上限到達時
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+];
