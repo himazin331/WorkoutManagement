18e1595384a71156cba326067fb8a20eb8336e71
himazin331 <sion20022013@gmail.com>
2021-09-07 04:20:36 +0900

    Webアプリモデル

---
+++ laravel/app/Models/EWMModel.php
@@ -0,0 +1,12 @@
+<?php
+//* Webアプリモデル */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EWMModel extends Model
+{
+    use HasFactory;
+}
